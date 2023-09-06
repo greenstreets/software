032f15ef4848aeafe43b23c17b1237685640ae64
aidenkim <akim1403@hotmail.com>
2023-09-06 11:24:42 +1000

    Testing mysql server

--- server/server.js
+++ server/server.js
@@ -3,14 +3,24 @@ const app = express();
 const cors = require("cors"); // Import the cors middleware
 const port = 8000; // You can change this to any available port you prefer
 app.use(cors()); // Use the cors middleware
+
 //////////////////////////DATABASE CONNECTION/////////////////////////////////////
 //mysql port 3306 || X protocol port 33060
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "YOUR PASSWORD HERE",
+  password: "root",
   database: "greenstreets",
+  port: 3310,
+});
+
+db.query("SELECT * FROM trash", (err, results) => {
+  if (err) {
+    console.error(err); // Log any error if it occurred
+  } else {
+    console.log(results); // Log the query results if there was no error
+  }
 });
 
 // Define a simple route
