c9d7a9a4880564447d8187004e3498eca87ea1f8
Lachlan Harnett <lachlanjharnett@gmail.com>
2023-09-10 12:50:19 +1000

    Added input validation functionality

---
+++ client/src/components/loginSuccess.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import "../css/loginSucess.css";
+
+const LoginSuccess = () => {
+  return (
+    <>
+        <div>
+            <div className="success">
+                <p>Success</p>
+            </div>
+        </div>
+    </>
+  );
+};
+
+export default LoginSuccess;
--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,11 +1,13 @@
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
+import LoginSuccess from "../components/footer";
 import "../css/login.css";
 import React, { useState } from 'react';
 
 function Login() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [showForm, setShowForm] = useState(true);
 
     const handleUsernameChange = (event) => {
         setUsername(event.target.value);
@@ -43,14 +45,68 @@ function Login() {
         return valid;
     };
 
+
+    // async function handleFormSubmit(event) {
+    //     event.preventDefault();
+    //     if (!/^\S+@\S+$/.test(event.target[1].value) !== true) {
+    //       if (event.target[0].value.trim().length >= 2) {
+    //         if (event.target[2].value.trim().length !== 0) {
+    //           const contactMessage = {
+    //             name: event.target[0].value,
+    //             email: event.target[1].value,
+    //             subject: event.target[2].value,
+    //             message: event.target[3].value,
+    //           };
+    //           setShowForm(false);
+    //           const response = await fetch(
+    //             "http://localhost:5000/api/contactmessage",
+    //             {
+    //               method: "POST",
+    //               headers: {
+    //                 "Content-Type": "application/json",
+    //               },
+    //               body: JSON.stringify(contactMessage),
+    //             }
+    //           );
+    //           await response;
+    //         }
+    //       }
+    //     }
+    //   }
+
+
     console.log("login");
-    const handleSubmit = (event) => {
+    async function handleSubmit (event) {
         event.preventDefault();
 
+        var userSuccess = validateUser(username);
+        var passSuccess = validatePass(password);
         console.log(validateUser(username))
         console.log(validatePass(password))
         console.log(username);
         console.log(password);
+
+        if (userSuccess && passSuccess) {
+            setShowForm(false);  
+        }
+            
+
+        const userInfo = {
+            username: username,
+            password: password
+          };
+
+        const response = await fetch(
+            "http://localhost:8000/api/login",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(userInfo),
+            }
+        );
+        await response;
     }
 
     return (
@@ -58,25 +114,29 @@ function Login() {
           <Navbar></Navbar>
             <div className="App">
                 <header className="App-header">
-                    <div className="centre-login">
-                        <h2>Login</h2>
-                        <form onSubmit = {handleSubmit}>
-                            <div>
-                               <input id="password" type="text" value={username} onChange={handleUsernameChange} placeholder='Username'>
+                    {showForm ? (
+                        <div className="centre-login">
+                            <h2>Login</h2>
+                            <form onSubmit = {handleSubmit}>
+                                <div>
+                                <input id="password" type="text" value={username} onChange={handleUsernameChange} placeholder='Username'>
 
+                                    </input> 
+                                </div>
+                                
+                                <div>
+                                <input id="username" type="password" value={password} onChange={handlePasswordChange} placeholder='Password'>
                                 </input> 
-                            </div>
-                            
-                            <div>
-                               <input id="username" type="password" value={password} onChange={handlePasswordChange} placeholder='Password'>
-                            </input> 
-                            </div>
-                            
-                            
-                            <button type='submit'>Submit</button> 
-                    
-                        </form>
-                    </div>
+                                </div>
+                                
+                                
+                                <button type='submit'>Submit</button> 
+                        
+                            </form>
+                        </div>
+                    ) : (
+                        <LoginSuccess></LoginSuccess>
+                    )}
                 </header>
             </div>
            <Footer></Footer>  
